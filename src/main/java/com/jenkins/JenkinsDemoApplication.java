26ea4917a6dca998699c305ecef431aea6c67f55
Baina Naresh <bainanaresh@gmail.com>
2020-01-06 23:28:17 +0530

    1st commit

---
+++ src/main/java/com/jenkins/JenkinsDemoApplication.java
@@ -0,0 +1,27 @@
+package com.jenkins;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDemoApplication {
+	
+	public static Logger logger=LoggerFactory.getLogger(JenkinsDemoApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("in init method");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		logger.info("in main method........");
+		SpringApplication.run(JenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/JenkinsDemoApplicationTests.java
@@ -0,0 +1,22 @@
+package com.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(JenkinsDemoApplication.class);
+
+	@Test
+	void contextLoads() {
+		
+		logger.info("in test class method.........");
+		assertEquals(true,true);
+		
+	}
+
+}
